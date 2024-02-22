7277b75d4fd0f5dee497902daebac149e7c30ea6
andyyu1104 <andyyu1104@gmail.com>
2024-02-22 11:48:00 +0000

    Finished boolean course about minesweeper

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//* --------------------
+//* Preparation Phrase
+//* --------------------
+
 //querySelector get the first element in the document
 const scoreCounter = document.querySelector('.score-counter');
 const grid = document.querySelector('.grid');
@@ -6,13 +10,60 @@ const endGameText = document.querySelector('.end-game-text');
 const playAgainButton = document.querySelector('.play-again');
 
 
-//Game states
+//Initialise variables needed for game setup
 const totalCells = 100;
-const totalBombs = 3;
-const maxScore = 5;
+const totalBombs = 6;
+const maxScore = totalCells - totalBombs;
 const bombsList = [];
 
 let score = 0;
+
+//a loop generating every cells
+for (let i = 1; i <= 100; i++) {
+    const cell = document.createElement('div');// cell = <div></div>
+    cell.classList.add('cell');//cell = <div class="cell"></div>
+
+    //When a player click a cell:
+    cell.addEventListener('click', function () {
+        //console.log(`You clicked on cell number ${i}`);// Testing // ` ${i}` we can use the i variable.
+
+        if (cell.classList.contains('cell-clicked')) { //avoid clicking a clicked cell
+            return;
+        }
+
+        if (bombsList.includes(i)) { //clicked on a bomb
+            cell.classList.add('cell-bomb');
+            endGame(false);
+        }
+        cell.classList.add('cell-clicked');
+        updateScore();
+
+
+
+    })
+
+    grid.appendChild(cell);
+}
+
+//generating the bomb's position
+while (bombsList.length < totalBombs) {
+    //Generate a random number between 1 and 100, inclusive
+    const randomNumber = Math.floor(Math.random() * totalCells) + 1; //the +1 is for the case of random() giving zero.
+
+    if (!bombsList.includes(randomNumber)) { //avoid a cell has more than one bomb
+        bombsList.push(randomNumber);
+    }
+
+}
+bombsList.sort();// not necessary but easy to know where the bomb are:3
+
+playAgainButton.addEventListener('click', function () {
+    window.location.reload();
+})
+
+//* -------------
+//* Functions
+//* -------------
 
 function updateScore() {
     score++;
@@ -23,37 +74,6 @@ function updateScore() {
     }
 }
 
-//a loop for generating every cells
-for (let i = 1; i <= 100; i++) {
-    const cell = document.createElement('div');// cell = <div></div>
-    cell.classList.add('cell');//cell = <div class="cell"></div>
-
-    cell.addEventListener('click', function () {
-        //console.log(`You clicked on cell number ${i}`);// Testing // ` ${i}` we can use the i variable.
-
-        if (bombsList.includes(i)) {
-            cell.classList.add('cell-bomb');
-            endGame(false);
-        } else if (!cell.classList.contains('cell-clicked')) {
-            cell.classList.add('cell-clicked');
-            updateScore();
-        }
-
-    })
-
-    grid.appendChild(cell);
-}
-
-while (bombsList.length < totalBombs) {
-    //Generate a random number between 1 and 100, inclusive
-    const randomNumber = Math.floor(Math.random() * totalCells) + 1; //the +1 is for the case of random() giving zero.
-
-    if (!bombsList.includes(randomNumber)) {
-        bombsList.push(randomNumber);
-    }
-
-}
-
 function endGame(isVictory) {
     if (isVictory) {
         endGameText.innerHTML = 'YOU<br />WON';
@@ -61,6 +81,7 @@ function endGame(isVictory) {
     } else {
         endGameText.innerHTML = 'YOU<br />LOSE';
         endGameScreen.classList.add('lose');
+        revealAllBombs();
     }
 
 
@@ -68,6 +89,16 @@ function endGame(isVictory) {
     endGameScreen.classList.remove('hidden');
 }
 
-playAgainButton.addEventListener('click', function () {
-    window.location.reload();
-})
+function revealAllBombs() {
+    const cells = document.querySelectorAll('.cell');
+
+    for (let i = 1; i <= cells.length; i++) {
+        const cell = cells[i - 1];
+        if (bombsList.includes(i)) {
+            cell.classList.add('cell-bomb');
+        }
+    }
+}
+
+
+
